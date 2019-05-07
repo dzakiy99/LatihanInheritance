e3010d582fb570b70559c23f1b9bf8b0a2ba3cec
dzakiy99 <thezakiy11@gmail.com>
2019-05-07 14:41:59 +0700

    inheritance

---
+++ LatihanInheritance/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "Anton";
+            person.Age = 19;
+            person.GetNameAndAge();
+
+            Teacher teacher = new Teacher();
+            teacher.Name = "Thomas";
+            teacher.Age = 35;
+            teacher.TeacherId = "2289";
+            teacher.Subject = "Programming";
+            teacher.GetNameAndAge();
+
+            Student student = new Student();
+            student.Name = "Abdul";
+            student.Age = 19;
+            student.StudentId = "1811";
+            student.Email = "abdul2000@gmail.com";
+            student.GetNameAndAge();
+
+            Console.ReadKey();
+        }
+    }
+}
